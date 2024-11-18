021144fa966d024b38a931b173c6651c77147179
dev-Shivam999 <shivamswami982@gmail.com>
2024-11-18 15:31:48 +0530

    add

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,59 @@
- import express, { Request, Response } from 'express'
-const app = express();
-const port = 3000;
-app.use(express.json())
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World');
+import express, { Request, Response } from "express";
+import { z } from "zod";
+import { client } from "./db";
+
+export const app = express();
+
+app.use(express.json());
+
+const sumInput = z.object({
+    a: z.number(),
+    b: z.number()
+})
+
+app.post("/sum", async (req: Request, res: Response) => {
+    const parsedResponse = sumInput.safeParse(req.body)
+
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const answer = await client.output.create({
+
+        data: {
+            a: parsedResponse.data.a,
+            b: parsedResponse.data.b,
+            Req: parsedResponse.data.a + parsedResponse.data.b
+
+        },
+        select: {
+            Req: true,
+        }
+    })
+
+    res.json({
+        answer
+    })
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:port`);
+app.get("/sum", (req: Request, res: Response) => {
+    const parsedResponse = sumInput.safeParse({
+        a: Number(req.headers["a"]),
+        b: Number(req.headers["b"])
+    })
+
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    res.json({
+        answer
+    })
 });
